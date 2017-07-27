fb7a99c68c76dd08d03144be50b6b8efb7e89d25
He Yong <yhe@vip.sina.com>
2017-07-27 21:25:47 +0800

    Create the Ruby file.
    This file is used to describe a Markof Link with Graph data struction.
    The Graph is saved as a state-trans matrix.
    The Matrix is saved in a cross-link list.

---
+++ MarkofGraph.rb
@@ -0,0 +1,12 @@
+#! /usr/bin/ruby -w
+
+BEGIN {
+    print("This program is used for Markof Link.\n");
+    print("Described by Graph and Matrix + Cross Link.\n");
+}
+
+END {
+    print("Program End.\n");
+}
+__END__
+2017.Jul.27
